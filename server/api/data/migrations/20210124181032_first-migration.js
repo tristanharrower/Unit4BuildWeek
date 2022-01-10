9f1b1f1e668a64c671ad7feb9747af2f6988cdac
[Tristan Harrower] <[harrowertristan@gmail.com]>
2022-01-10 17:32:21 -0500

    added guests table

--- server/api/data/migrations/20210124181032_first-migration.js
+++ server/api/data/migrations/20210124181032_first-migration.js
@@ -8,13 +8,26 @@ exports.up = async (knex) => {
     })
 
     await knex.schema
+    .createTable('guests', (guests) => {
+      guests.increments('guest_id')
+      guests.string('username', 200).notNullable()
+      guests.string('password', 200).notNullable()
+      guests.timestamps(false, true)
+    })
+
+    await knex.schema
       .createTable('potlucks', (potlucks) => {
         potlucks.increments('potluck_id')
         potlucks.integer('organizer_id')
         .unsigned()
-        .notNullable()
         .references('organizer_id')
         .inTable('organizers')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE')
+        potlucks.integer('guest_id')
+        .unsigned()
+        .references('guest_id')
+        .inTable('guests')
         .onDelete('CASCADE')
         .onUpdate('CASCADE')
         potlucks.string('event_name', 128).notNullable()
@@ -34,12 +47,26 @@ exports.up = async (knex) => {
         .inTable('potlucks')
         .onDelete('CASCADE')
         .onUpdate('CASCADE')
+        foods.integer('organizer_id')
+        .unsigned()
+        .references('organizer_id')
+        .inTable('organizers')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE')
+        foods.integer('guest_id')
+        .unsigned()
+        .references('guest_id')
+        .inTable('guests')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE')
         foods.string('food_wanted').notNullable()
       })
 }
 
 exports.down = async (knex) => {
   await knex.schema.dropTableIfExists('organizers')
+  await knex.schema.dropTableIfExists('guests')
   await knex.schema.dropTableIfExists('potlucks')
   await knex.schema.dropTableIfExists('foods')
+
 }
