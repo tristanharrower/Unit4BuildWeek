c73ab8e19eaaa2968982570247677b8400533c5a
[Tristan Harrower] <[harrowertristan@gmail.com]>
2022-02-09 16:16:09 -0500

    added type to apimessages

--- api/messages/messages_model.js
+++ api/messages/messages_model.js
@@ -2,7 +2,8 @@ const db = require('../data/db-config');
 
 async function createMessage(message) {
     const [newMessage] = await db('messages').insert(message,
-        ['message_id',  'organizer_id', 'attendee_id', 'attendee_username', 'potluck_id'] )
+        ['message_id',  'organizer_id', 'attendee_id', 
+        'attendee_username','messages.type', 'potluck_id'] )
     return newMessage 
   }
   
@@ -14,14 +15,14 @@ async function createMessage(message) {
       const messages = await db("messages")
       .join('potlucks', 'messages.potluck_id', '=', 'potlucks.potluck_id')
       .select('messages.message_id','potlucks.potluck_id','potlucks.username',  'messages.attendee_id',
-         'messages.attendee_username','potlucks.event_name', 'potlucks.description','potlucks.event_date', 
+         'messages.attendee_username','messages.type', 'potlucks.event_name', 'potlucks.description','potlucks.event_date', 
          'potlucks.event_time', 'potlucks.location')
          return messages
     }else {
       const messages = await db("messages")
       .join('potlucks', 'messages.potluck_id', '=', 'potlucks.potluck_id')
       .select('messages.message_id','potlucks.potluck_id', 'potlucks.username','messages.attendee_id',
-      'messages.attendee_username','potlucks.event_name', 'potlucks.description','potlucks.event_date', 
+      'messages.attendee_username', 'messages.type', 'potlucks.event_name', 'potlucks.description','potlucks.event_date', 
          'potlucks.event_time', 'potlucks.location')
       .where(`messages.${filterKey[0]}`, filterValue[0])
         return messages;
